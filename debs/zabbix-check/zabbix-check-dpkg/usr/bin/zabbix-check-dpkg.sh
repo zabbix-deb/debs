3caf7a9af4f86b7719cd0983128809bb11c122d3
Christoph Hueffelmann <christoph.hueffelmann@cubos-internet.de>
2015-01-26 15:30:50 +0100

    zabbix-check-dpkg (0.1~devi5) precise; urgency=low

--- debs/zabbix-check/zabbix-check-dpkg/usr/bin/zabbix-check-dpkg.sh
+++ debs/zabbix-check/zabbix-check-dpkg/usr/bin/zabbix-check-dpkg.sh
@@ -5,15 +5,31 @@ share="/usr/share/zabbix/dpkg"
 mkdir -p $share
 echo 0 | tee $share/error.log | tee $share/updates.log > $share/security.log
 
-tmp=$(mktemp)
-trap "rm -f $tmp" 0 1 2 5 15
+function checkrun() {
+	err=0
+	for i in {0..5}; do 
+		apt-get check >/dev/null 2>&1
+		err=$?
+		if [ "$err" -eq "0" ]; then
+			return 0
+		fi
+		sleep 360
+	done
+	echo $err > $share/error.log
+	exit 1
+}
 
 #check runig updates
-apt-get check >/dev/null || sleep 360
+checkrun
+
 #update database
-apt-get update >/dev/null || (echo $? > $share/error.log; exit 1)
+apt-get update >/dev/null 2>&1 || (echo $? > $share/error.log; exit 1)
+
 #check updates
+tmp=$(mktemp)
+trap "rm -f $tmp" 0 1 2 5 15
 apt-get upgrade -s 2>/dev/null > $tmp || (echo $? > $share/error.log; exit 2)
 grep -vi security $tmp | grep ^Inst | wc -l > $share/updates.log
 grep -i security $tmp | grep ^Inst | wc -l > $share/security.log
+
 exit 0
