99fd3cfa623b122ea1959f95b070679f916ca25d
Christoph Hueffelmann <christoph.hueffelmann@cubos-internet.de>
2015-08-19 11:46:56 +0200

    zabbix-check-ssl-notafter.sh: bugfix error

--- externalscripts/ssl/zabbix-check-ssl-notafter.sh
+++ externalscripts/ssl/zabbix-check-ssl-notafter.sh
@@ -3,16 +3,24 @@
 domain=$1
 port=$2
 if [ "$domain" = "" ]; then
-	echo "$0 domain.tld [port]"
-	exit 1
+        echo "$0 domain.tld [port]"
+        exit 1
 fi
 if [ "$port" = "" ]; then
-	port="443"
+        port="443"
 fi
 
-d=$(echo | openssl s_client -servername $domain -connect $domain:$port 2>/dev/null | openssl x509 -noout -dates | grep notA | awk -F'=' '{print$2}')
+tmp=$(mktemp)
+echo | openssl s_client -servername $domain -connect $domain:$port 2>/dev/null > $tmp
 
-let t=$(date -d "$d" "+%s")-$(date "+%s")
-echo $t
+if [ -s $tmp ]; then
+        d=$(cat $tmp | openssl x509 -noout -dates | grep notA | awk -F'=' '{print$2}')
 
-exit 0
+        let t=$(date -d "$d" "+%s")-$(date "+%s")
+        echo $t
+
+        exit 0
+fi
+echo 0
+exit 1
+
