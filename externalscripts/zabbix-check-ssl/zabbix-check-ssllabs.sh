3a2cbeaa87e25dc59b2e571a2a974251d6e5432e
Christoph Hueffelmann <christoph.hueffelmann@cubos-internet.de>
2015-11-05 12:20:04 +0100

    zabbix-check-ssllabs.sh: add trap

--- externalscripts/zabbix-check-ssl/zabbix-check-ssllabs.sh
+++ externalscripts/zabbix-check-ssl/zabbix-check-ssllabs.sh
@@ -5,7 +5,7 @@ if [ "$domain" = "" ]; then
 fi
 
 tmp=$(mktemp)
-#TODO: trap
+trap "rm -f $tmp" 0 1 2 5 15
 wget -q -O $tmp "https://api.dev.ssllabs.com/api/fa78d5a4/analyze?host=$domain&#38;publish=On&#38;clearCache=On&#38;all=done"
 sed 's/,/\n/g' $tmp | grep grade | awk -F '"' '{print $4}'
 rm $tmp
