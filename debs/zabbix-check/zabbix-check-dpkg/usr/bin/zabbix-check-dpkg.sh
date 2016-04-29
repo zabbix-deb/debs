e924635da8dbeeddbdf94de5c09fea42a51ae00d
Christoph Hueffelmann <chr@istoph.de>
2016-04-29 11:05:03 +0200

    zabbix-check-dpkg (0.1~dev14):* Bugfix: Post-Invoke * change: mv crontab jobs into backgrund

--- debs/zabbix-check/zabbix-check-dpkg/usr/bin/zabbix-check-dpkg.sh
+++ debs/zabbix-check/zabbix-check-dpkg/usr/bin/zabbix-check-dpkg.sh
@@ -1,43 +1,53 @@
 #!/bin/bash
 export LANG=C
 
-share="/usr/share/zabbix/dpkg"
-mkdir -p $share
-echo 0 | tee $share/error.log | tee $share/updates.log | tee $share/autoremove.log > $share/security.log 
+function run() {
+	share="/usr/share/zabbix/dpkg"
+	mkdir -p $share
+	echo 0 | tee $share/error.log | tee $share/updates.log | tee $share/autoremove.log > $share/security.log 
 
-function checkrun() {
-	err=0
-	for i in {0..5}; do 
-		apt-get check >/dev/null 2>&1
-		err=$?
-		if [ "$err" -eq "0" ]; then
-			return 0
-		fi
-		sleep 360
-	done
-	echo $err > $share/error.log
-	exit 1
+	function checkrun() {
+		err=0
+		for i in {0..5}; do 
+			apt-get check >/dev/null 2>&1
+			err=$?
+			if [ "$err" -eq "0" ]; then
+				return 0
+			fi
+			sleep 360
+		done
+		echo $err > $share/error.log
+		exit 1
+	}
+
+	#reboot required
+	if [ -e /var/run/reboot-required ]; then
+	        echo 1 > $share/reboot.log
+	else
+	        echo 0 > $share/reboot.log
+	fi
+
+	#check runig updates
+	checkrun
+
+	#update database
+	apt-get update >/dev/null 2>&1 || (echo $? > $share/error.log; exit 1)
+
+	#check updates
+	tmp=$(mktemp)
+	trap "rm -f $tmp" 0 1 2 5 15
+	apt-get upgrade -s 2>/dev/null > $tmp || (echo $? > $share/error.log; exit 2)
+	grep -vi security $tmp | grep ^Inst | wc -l > $share/updates.log
+	grep -i security $tmp | grep ^Inst | wc -l > $share/security.log
+	apt-get autoremove -s 2>/dev/null | grep ^Remv | wc -l > $share/autoremove.log
 }
 
-#reboot required
-if [ -e /var/run/reboot-required ]; then
-        echo 1 > $share/reboot.log
-else
-        echo 0 > $share/reboot.log
+if [ "$1" == "Post-Invoke" ]; then
+	(sleep 360; run) &
+elif ["$1" == "cron"  ]; then
+	(perl -e 'sleep rand(3000)'; run) &
+else 
+	run
 fi
-
-#check runig updates
-checkrun
-
-#update database
-apt-get update >/dev/null 2>&1 || (echo $? > $share/error.log; exit 1)
-
-#check updates
-tmp=$(mktemp)
-trap "rm -f $tmp" 0 1 2 5 15
-apt-get upgrade -s 2>/dev/null > $tmp || (echo $? > $share/error.log; exit 2)
-grep -vi security $tmp | grep ^Inst | wc -l > $share/updates.log
-grep -i security $tmp | grep ^Inst | wc -l > $share/security.log
-apt-get autoremove -s 2>/dev/null | grep ^Remv | wc -l > $share/autoremove.log
 
 exit 0
