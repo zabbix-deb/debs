28e5f47d399e193426ccb428ebb6abe4905fd833
Christoph Hueffelmann <christoph.hueffelmann@cubos-internet.de>
2016-11-29 15:42:58 +0100

    zabbix-check-disk-performance (0.1~dev1) * Initial release.

---
+++ debs/zabbix-check/zabbix-check-disk-performance/usr/lib/zabbix-check/bin/zabbix-check-disk-performance-discover.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import json
+import subprocess
+
+returnvalue = { "data" : [] }
+p = subprocess.Popen('./zabbix-check-disk-performance-discover', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+for hdd in p.stdout.readlines():
+    returnvalue["data"].append({ "{#DEVICENAME}" : hdd.strip() })
+print json.dumps(returnvalue)
+
