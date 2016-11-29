be45aeab6a1b1832e7099e01bbc84bccd6581a47
Christoph Hueffelmann <chr@istoph.de>
2016-11-29 23:59:48 +0100

    zabbix-check-disk-performance (0.1~dev2) * Add scsi support * Bugfixes

--- debs/zabbix-check/zabbix-check-disk-performance/usr/lib/zabbix-check/bin/zabbix-check-disk-performance-discover.py
+++ debs/zabbix-check/zabbix-check-disk-performance/usr/lib/zabbix-check/bin/zabbix-check-disk-performance-discover.py
@@ -3,7 +3,7 @@ import json
 import subprocess
 
 returnvalue = { "data" : [] }
-p = subprocess.Popen('./zabbix-check-disk-performance-discover', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+p = subprocess.Popen('/usr/lib/zabbix-check/bin/zabbix-check-disk-performance-discover', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 for hdd in p.stdout.readlines():
     returnvalue["data"].append({ "{#DEVICENAME}" : hdd.strip() })
 print json.dumps(returnvalue)
