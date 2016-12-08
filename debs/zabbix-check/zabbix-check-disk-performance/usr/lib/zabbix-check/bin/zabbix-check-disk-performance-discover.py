a9d5755bb946c5a4c918bc2849f78985f9e73498
Sebastian Huebner <sebastian@hueb-ner.de>
2016-12-08 13:55:01 +0100

    zabbix-check-disk-performance: change dm regex and can so remove the split('dm-name-')

--- debs/zabbix-check/zabbix-check-disk-performance/usr/lib/zabbix-check/bin/zabbix-check-disk-performance-discover.py
+++ debs/zabbix-check/zabbix-check-disk-performance/usr/lib/zabbix-check/bin/zabbix-check-disk-performance-discover.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+
 import json
 import os
 import re
@@ -6,7 +7,7 @@ import re
 disk_id_path = "/dev/disk/by-id/"
 dev_list = []
 dev_re = re.compile(r'^.*(ata|scsi)+(?!.*?part).*$')
-dm_re = re.compile(r'^dm-name.*$')
+dm_re = re.compile(r'^dm-name-(.*)$')
 
 for name in os.listdir(disk_id_path):
     dev_match = dev_re.match(name)
@@ -16,7 +17,7 @@ for name in os.listdir(disk_id_path):
         if real_dev != "sr0":
             dev_list.append(real_dev)
     elif dm_match:
-        dev_list.append(dm_match.group(0).split('dm-name-')[1])
+        dev_list.append(dm_match.group(1))
 
 returnvalue = { "data" : [] }
 for dev in dev_list:
