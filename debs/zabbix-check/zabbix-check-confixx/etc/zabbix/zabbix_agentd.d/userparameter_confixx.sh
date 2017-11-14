777a0ff7d57a54854f6cd1a7fc3cab8605830150
Christoph Hueffelmann <christoph.hueffelmann@cubos-internet.de>
2017-11-14 10:39:26 +0100

    zabbix-check-confixx (0.1~dev3) * Add SN and Key Value * Chech chmod parameters

--- debs/zabbix-check/zabbix-check-confixx/etc/zabbix/zabbix_agentd.d/userparameter_confixx.sh
+++ debs/zabbix-check/zabbix-check-confixx/etc/zabbix/zabbix_agentd.d/userparameter_confixx.sh
@@ -1,2 +1,4 @@
 EnableRemoteCommands=1
 UserParameter=confixx.lizenz,cat /usr/share/zabbix/confixx/lizenz
+UserParameter=confixx.sn,cat /usr/share/zabbix/confixx/sn
+UserParameter=confixx.key,cat /usr/share/zabbix/confixx/key
